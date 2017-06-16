ef1685f3161bb78ec8f2b3766dc9f5a31c9b25be
Furqan <furqan_shaikh_1999@hotmail.com>
2017-06-16 16:06:02 +0530

    Added Merge.bat

---
+++ Merge.bat
@@ -0,0 +1,17 @@
+REM STEPS For Merging
+
+REM Lets say we want to merge from LandingPage to Dev
+
+REM 1. Merge is always to be done on local git Repo
+
+
+REM git checkout <dest_branch>
+REM get latest for <dest_branch>
+REM git merge <source_branch>
+REM Resolve merge conflicts, if any
+REM Push to origin
+
+
+
+
+
