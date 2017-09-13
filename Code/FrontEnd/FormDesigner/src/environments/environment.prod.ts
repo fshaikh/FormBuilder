e7e360e47d21aa37ab5cb75f32a01f3ffa4a9b06
Furqan <furqan_shaikh_1999@hotmail.com>
2017-09-13 18:49:44 +0530

    [DEV] Added source map when building. Added linting. Added support for deploying to Cloud Foundry [COMPLETED]

--- Code/FrontEnd/FormDesigner/src/environments/environment.prod.ts
+++ Code/FrontEnd/FormDesigner/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  serverUrl:'http://ec2-52-34-28-89.us-west-2.compute.amazonaws.com/api'
+  //serverUrl:'http://ec2-52-34-28-89.us-west-2.compute.amazonaws.com/api'
+  serverUrl:'https://formbuilder-dev.cfapps.io/api'
 };
