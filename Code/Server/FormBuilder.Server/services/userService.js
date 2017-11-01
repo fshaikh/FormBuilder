42387f39322c1047b3d3ebae30e31e4edba91bc8
Furqan Shaikh <furqan_shaikh_1999@hotmail.com>
2017-11-01 08:12:11 +0530

    [DEV] Added userName property for guest authentication. Added HammerJS [COMPLETED]

--- Code/Server/FormBuilder.Server/services/userService.js
+++ Code/Server/FormBuilder.Server/services/userService.js
@@ -12,6 +12,7 @@ module.exports = (function () {
         // b. If userid is blank, implies new user.So generate a random unique userid
         if (!userId) {
             user.userId = utilService.getUniqueId();
+            user.userName = 'guest_' + user.userId;
             // c. Save the new user in users collection in MongoDb
             let userDA = new userDAL.UserDataAccess();
             let response = await userDA.addUser(user);
